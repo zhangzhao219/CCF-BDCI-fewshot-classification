846f44706f7c35d7e793d8aac8c5586adfd4b42f
Yiming Li <47707075+Ming-er@users.noreply.github.com>
2022-10-17 09:11:41 +0800

    Update add_pseudo_labels.py
    fix some bugs

--- add_pseudo_labels.py
+++ add_pseudo_labels.py
@@ -14,7 +14,7 @@ parser.add_argument('--corpus_json', type=str, required=True, help='Test json fi
 parser.add_argument('--origin_train_json', type=str, required=True, help='Train json file')
 parser.add_argument('--data_folder_dir', type=str, required=True, help='Data Folder Location')
 parser.add_argument('--fix_thresh', type=float, default=0.70, help='Fixed thresh')
-parser.add_argument('--percent', type=float, default=10, help='top percent confidence score will be regarded as reliable pseudo labels for a given class')
+parser.add_argument('--percent', type=float, default=50, help='top (100% - percent) confidence score will be regarded as reliable pseudo labels for a given class')
 
 args = parser.parse_args()
 
@@ -31,9 +31,9 @@ class_thresh = []
 for l in range(num_labels):
     sub_df = df[df['label'] == l]
     sub_score = np.array(sub_df['score'])
-    thresh = np.percentile(sub_score, 30)
-    if thresh < 0.7:
-        thresh = 0.7
+    thresh = np.percentile(sub_score, args.percent)
+    if thresh < args.fix_thresh:
+        thresh = args.fix_thresh
     sub_df = sub_df.drop(sub_df[sub_df['score'] < thresh].index)
     pseudo_label_df = pd.concat([pseudo_label_df, sub_df])
 
