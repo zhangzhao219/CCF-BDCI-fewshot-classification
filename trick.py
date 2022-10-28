36b5b6c566060d1f6666159b833b6e5da21f6766
Yiming Li <47707075+Ming-er@users.noreply.github.com>
2022-10-28 23:10:11 +0800

    add awp
    add awp

--- trick.py
+++ trick.py
@@ -128,3 +128,61 @@ class EMA:
                 assert name in self.backup
                 param.data = self.backup[name]
         self.backup = {}
+# AWP        
+class AWP:
+    """
+    Implements weighted adverserial perturbation
+    adapted from: https://www.kaggle.com/code/wht1996/feedback-nn-train/notebook
+    """
+
+    def __init__(self, model, start_epoch, adv_param="weight", adv_lr=0.0005, adv_eps=0.001):
+        self.model = model
+        self.adv_param = adv_param
+        self.adv_lr = adv_lr
+        self.adv_eps = adv_eps
+        self.start_epoch = start_epoch
+        self.backup = {}
+        self.backup_eps = {}
+
+    def attack_backward(self, input_ids, token_type_ids, attention_mask, epoch, labels, criterion):
+        if (self.adv_lr == 0) or (epoch < self.start_epoch):
+            return
+        self._save()
+        self._attack_step()
+
+        y_preds = self.model(input_ids, token_type_ids, attention_mask)
+
+        adv_loss = criterion(y_preds, labels)
+        self.model.zero_grad()
+        return adv_loss
+
+    def _attack_step(self):
+        e = 1e-6
+        for name, param in self.model.named_parameters():
+            if param.requires_grad and param.grad is not None and self.adv_param in name:
+                norm1 = torch.norm(param.grad)
+                norm2 = torch.norm(param.data.detach())
+                if norm1 != 0 and not torch.isnan(norm1):
+                    r_at = self.adv_lr * param.grad / (norm1 + e) * (norm2 + e)
+                    param.data.add_(r_at)
+                    param.data = torch.min(
+                        torch.max(param.data, self.backup_eps[name][0]), self.backup_eps[name][1]
+                    )
+
+    def _save(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad and param.grad is not None and self.adv_param in name:
+                if name not in self.backup:
+                    self.backup[name] = param.data.clone()
+                    grad_eps = self.adv_eps * param.abs().detach()
+                    self.backup_eps[name] = (
+                        self.backup[name] - grad_eps,
+                        self.backup[name] + grad_eps,
+                    )
+
+    def restore(self,):
+        for name, param in self.model.named_parameters():
+            if name in self.backup:
+                param.data = self.backup[name]
+        self.backup = {}
+        self.backup_eps = {}
