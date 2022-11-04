4bad5e6ce9a1709e888a26b9382986638c9489fb
zhangzhao219 <zhaozhao809@163.com>
2022-11-04 22:53:01 +0800

    add ensemble code

--- generate_sh.py
+++
@@ -1,6 +0,0 @@
-import os
-
-with open('run.sh','w') as f:
-    for i in os.listdir('./runsh'):
-        for j in os.listdir('./runsh/' + i):
-            f.writelines('bash ./runsh/' + i + '/' + j + '\n')
